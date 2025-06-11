d747dca05d821fd8d7f16047d841f14dfa467480
Rafael Gutkowski <nounderline@libred.org>
2025-06-11 18:31:13 -0400

    Opt-in signals & render export
    SIgnals modify Praect singleton. Better to keep it opt in under its own export.

---
+++ render.ts
@@ -0,0 +1,9 @@
+export {
+  renderToStaticMarkup,
+  renderToString,
+  renderToStringAsync,
+} from "./packages/preact-render-to-string/src/index.js"
+
+export {
+  renderToReadableStream,
+} from "./packages/preact-render-to-string/src/stream.js"
---
+++ signals.ts
@@ -0,0 +1,3 @@
+export * from "./packages/signals-preact/src/index.ts"
+
+export * from "./packages/signals-preact/utils/src/index.ts"
